c8373b1747e802afcae9c1c2c66359234d9d6fce
CarolinaRojas <carolirojas73@gmail.com>
2024-06-20 17:51:50 -0400

    todojunto-botones

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
             <div className="Container">
                 <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
                 <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown" />
-                <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown" />
-                <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown" />
+                
             </div>
         </div>
     );
--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,12 +1,22 @@
 // CAROLINA ROJAS COLLANTE
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age);
+
+    const handleBirthday = () => {
+        setAge(age + 1);
+    };
+
     return (
         <div>
             <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            {(props.firstName === 'John' && props.lastName === 'Smith') ||
+            (props.firstName === 'Jane' && props.lastName === 'Doe') ? (
+                <button onClick={handleBirthday}>Birthday Button for {props.firstName} {props.lastName}</button>
+            ) : null}
         </div>
     );
 };
